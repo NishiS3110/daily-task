98eae399c30e761182614dff232db2126801174f
NishiS3110 <403hostu19ivlaleg@gmail.com>
2019-02-17 20:15:22 +0900

    インデントの修正

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,17 +1,17 @@
 <template>
-    <v-toolbar>
-      <v-toolbar-items>
-        <v-btn flat @click="goToTasKList">タスク一覧</v-btn>
-        <v-btn flat @click="gotToProfile">プロフィール</v-btn>
-      </v-toolbar-items>
-      <v-spacer></v-spacer>
+  <v-toolbar>
+    <v-toolbar-items>
+      <v-btn flat @click="goToTasKList">タスク一覧</v-btn>
+      <v-btn flat @click="gotToProfile">プロフィール</v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
 
-      <!-- あとで追加あるかも -->
-      <!-- <v-toolbar-items>
-        <v-btn flat>ログイン</v-btn>
-        <v-btn flat>ログアウト</v-btn>
-      </v-toolbar-items> -->
-    </v-toolbar>
+    <!-- あとで追加あるかも -->
+    <!-- <v-toolbar-items>
+      <v-btn flat>ログイン</v-btn>
+      <v-btn flat>ログアウト</v-btn>
+    </v-toolbar-items> -->
+  </v-toolbar>
 </template>
 
 <script>
--- src/components/viewModel/AddTask.vue
+++ src/components/viewModel/AddTask.vue
@@ -1,68 +1,72 @@
 <template>
-<v-dialog v-model="dialog" max-width="450px">
-        <v-btn slot="activator" color="red" dark>タスクの追加</v-btn>
-        <v-card>
-          <v-card-title>
-            <span class="headline">タスク</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex xs12>
-                  <v-text-field 
-                    v-model="title"
-                    label="タイトル*" 
-                    required>
-                  </v-text-field>
-                </v-flex>
-                <v-flex xs12>
-                  <v-textarea
-                    v-model="detail"
-                    label="詳細"
-                  >
-                  </v-textarea>
-                </v-flex>
-                <v-flex xs12>
-                  <v-menu
-                    ref="menu"
-                    v-model="menu"
-                    :close-on-content-click="false"
-                    :nudge-left="nudgeLeft"
-                    :return-value.sync="date"
-                    lazy
-                    transition="scale-transition"
-                    offset-y
-                    full-width
-                    min-width="290px"
-                  >
-                    <v-text-field
-                      slot="activator"
-                      v-model="date"
-                      label="*完了日"
-                      prepend-icon="event"
-                      readonly
-                    ></v-text-field>
-                    <v-date-picker v-model="date" no-title scrollable>
-                      <v-spacer></v-spacer>
-                      <v-btn flat color="primary" @click="menu = false">閉じる</v-btn>
-                      <v-btn flat color="primary" @click="$refs.menu.save(date)">決定</v-btn>
-                    </v-date-picker>
-                  </v-menu>
-                </v-flex>
-                <v-flex xs12>
-                  <v-switch v-model="completed" :label="displayCompleted"></v-switch>
-                </v-flex>
-              </v-layout>
-            </v-container>
-            <small>*は必須入力です</small>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="dialog = false">閉じる</v-btn>
-            <v-btn color="blue darken-1" flat @click="saveTask">保存</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+  <v-dialog v-model="dialog" max-width="450px">
+    <v-btn 
+      slot="activator" 
+      color="red" 
+      dark>タスクの追加
+    </v-btn>
+    <v-card>
+      <v-card-title>
+        <span class="headline">タスク</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container grid-list-md>
+          <v-layout wrap>
+            <v-flex xs12>
+              <v-text-field 
+                v-model="title"
+                label="タイトル*" 
+                required>
+              </v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-textarea
+                v-model="detail"
+                label="詳細"
+              >
+              </v-textarea>
+            </v-flex>
+            <v-flex xs12>
+              <v-menu
+                ref="menu"
+                v-model="menu"
+                :close-on-content-click="false"
+                :nudge-left="nudgeLeft"
+                :return-value.sync="date"
+                lazy
+                transition="scale-transition"
+                offset-y
+                full-width
+                min-width="290px"
+              >
+                <v-text-field
+                  slot="activator"
+                  v-model="date"
+                  label="*完了日"
+                  prepend-icon="event"
+                  readonly
+                ></v-text-field>
+                <v-date-picker v-model="date" no-title scrollable>
+                  <v-spacer></v-spacer>
+                  <v-btn flat color="primary" @click="menu = false">閉じる</v-btn>
+                  <v-btn flat color="primary" @click="$refs.menu.save(date)">決定</v-btn>
+                 </v-date-picker>
+              </v-menu>
+            </v-flex>
+            <v-flex xs12>
+              <v-switch v-model="completed" :label="displayCompleted"></v-switch>
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <small>*は必須入力です</small>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" flat @click="dialog = false">閉じる</v-btn>
+        <v-btn color="blue darken-1" flat @click="saveTask">保存</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
