9e84812b4e81cf4f509b68d9d21fc2328748897b
NishiS3110 <403hostu19ivlaleg@gmail.com>
2019-03-03 18:11:47 +0900

    firebaseの設定ファイル

---
+++ src/plugins/firebase.js
@@ -0,0 +1,19 @@
+import firebaseApp from 'firebase/app';
+import firebase from 'firebase'
+
+// Initialize Firebase
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+firebaseApp.initializeApp(config);
+
+const db = firebase.firestore()
+
+export {
+  db
+}
